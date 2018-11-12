c7218e183a701612ddaee08b0306d22b4f362614
Ricardo Brandan <brandanricardo@yahoo.com.ar>
2018-11-11 23:05:51 -0300

    Agraga pruebas de mapa. Faltan mas pruebas

--- src/atenea/fiuba/algoIII/ageoOfEmpires/Posicion.java
+++ src/atenea/fiuba/algoIII/ageoOfEmpires/Posicion.java
@@ -16,4 +16,17 @@ public class Posicion {
     public int getEjeY(){
         return this.ejeY;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Posicion)) {
+            return false;
+        }
+        if (obj == this) {
+            return true;
+        }
+        Posicion other = (Posicion) obj;
+
+        return (this.getEjeX() == other.getEjeX() && this.getEjeY() == other.getEjeY());
+    }
 }
