640b217c930f05580c2029098d4c470a269cbd98
Ricardo Brandan <brandanricardo@yahoo.com.ar>
2018-11-13 23:10:01 -0300

    Agrego mas pruebas de tamanio de mapa

---
+++ src/atenea/fiuba/algoIII/ageoOfEmpires/DimensionDelMapaInvalidaException.java
@@ -0,0 +1,12 @@
+package atenea.fiuba.algoIII.ageoOfEmpires;
+
+public class DimensionDelMapaInvalidaException extends RuntimeException {
+
+    public DimensionDelMapaInvalidaException(){
+        super();
+    }
+
+    public DimensionDelMapaInvalidaException(String mensaje){
+        super(mensaje);
+    }
+}
