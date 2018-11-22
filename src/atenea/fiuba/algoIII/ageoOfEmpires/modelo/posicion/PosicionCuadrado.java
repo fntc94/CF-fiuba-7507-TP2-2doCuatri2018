fa76a44ce565c46b1b14fdaf8b70561414563b87
Ricardo Brandan <brandanricardo@yahoo.com.ar>
2018-11-22 16:00:15 -0300

    Hace abstracta clase que no implementa comportamiento de clase base

--- src/atenea/fiuba/algoIII/ageoOfEmpires/modelo/posicion/PosicionCuadrado.java
+++ src/atenea/fiuba/algoIII/ageoOfEmpires/modelo/posicion/PosicionCuadrado.java
@@ -1,6 +1,6 @@
 package atenea.fiuba.algoIII.ageoOfEmpires.modelo.posicion;
 
-public class PosicionCuadrado extends Posicion {
+public abstract class PosicionCuadrado extends Posicion {
 
     public PosicionCuadrado(int xInicial, int yInicial , int xFinal, int yFinal) {
         super();
@@ -8,11 +8,5 @@ public class PosicionCuadrado extends Posicion {
         for(int j = yInicial; j >= yFinal; j--)
             for(int i = xInicial; i <= xFinal; i++)
                 this.listaCasilleros.add(new Casillero(i, j));
-    }
-
-    //  Una unidad que ocupa varios casilleros no puede moverse
-    @Override
-    public Posicion modificarCoordenada(int i, int i1) {
-        return new PosicionCuadrado(0,0,0,0);
     }
 }
