a52a454b9d9c22a6ea9393184f10d8b6b538b7aa
Ricardo Brandan <brandanricardo@yahoo.com.ar>
2018-11-22 16:07:35 -0300

    Arregla error

--- src/atenea/fiuba/algoIII/ageoOfEmpires/modelo/posicion/PosicionCuadrado.java
+++ src/atenea/fiuba/algoIII/ageoOfEmpires/modelo/posicion/PosicionCuadrado.java
@@ -1,6 +1,6 @@
 package atenea.fiuba.algoIII.ageoOfEmpires.modelo.posicion;
 
-public abstract class PosicionCuadrado extends Posicion {
+public class PosicionCuadrado extends Posicion {
 
     public PosicionCuadrado(int xInicial, int yInicial , int xFinal, int yFinal) {
         super();
@@ -8,5 +8,11 @@ public abstract class PosicionCuadrado extends Posicion {
         for(int j = yInicial; j >= yFinal; j--)
             for(int i = xInicial; i <= xFinal; i++)
                 this.listaCasilleros.add(new Casillero(i, j));
+    }
+
+
+    @Override
+    public Posicion modificarCoordenada(int i, int i1) {
+        return new PosicionCuadrado(0,0,0,0);
     }
 }
