85d34fd0d6d8407493b9ced1db855aef3d65d2e9
Ricardo Brandan <brandanricardo@yahoo.com.ar>
2018-11-26 23:27:27 -0300

    Posiciona cosas en lugares predefinidos

--- src/atenea/fiuba/algoIII/ageoOfEmpires/modelo/juego/Jugador.java
+++ src/atenea/fiuba/algoIII/ageoOfEmpires/modelo/juego/Jugador.java
@@ -1,17 +1,14 @@
 package atenea.fiuba.algoIII.ageoOfEmpires.modelo.juego;
 
 import atenea.fiuba.algoIII.ageoOfEmpires.modelo.Edificio;
-import atenea.fiuba.algoIII.ageoOfEmpires.modelo.edificios.Castillo;
-import atenea.fiuba.algoIII.ageoOfEmpires.modelo.edificios.EdificiosFabrica;
-import atenea.fiuba.algoIII.ageoOfEmpires.modelo.unidades.Plebe;
-import atenea.fiuba.algoIII.ageoOfEmpires.modelo.unidades.Ejercito;
-import atenea.fiuba.algoIII.ageoOfEmpires.modelo.unidades.Aldeano;
-import atenea.fiuba.algoIII.ageoOfEmpires.modelo.unidades.UnidadMilitar;
-import atenea.fiuba.algoIII.ageoOfEmpires.modelo.edificios.Construcciones;
-import atenea.fiuba.algoIII.ageoOfEmpires.modelo.edificios.PlazaCentral;
+import atenea.fiuba.algoIII.ageoOfEmpires.modelo.Unidad;
+import atenea.fiuba.algoIII.ageoOfEmpires.modelo.edificios.*;
+import atenea.fiuba.algoIII.ageoOfEmpires.modelo.posicion.*;
+import atenea.fiuba.algoIII.ageoOfEmpires.modelo.unidades.*;
 import atenea.fiuba.algoIII.ageoOfEmpires.modelo.excepciones.OroInsuficienteException;
 
 public class Jugador {
+    private Mapa mapa;
     private Plebe plebe;
     private Construcciones construcciones;
     private Ejercito ejercito;
@@ -20,8 +17,8 @@ public class Jugador {
     private static final int oroInicial = 100;
 
 
-    public Jugador() {
-
+    public Jugador(Mapa mapa) {
+        this.mapa = mapa;
         this.plebe = new Plebe();
         this.construcciones = new Construcciones();
         this.ejercito = new Ejercito();
@@ -29,14 +26,81 @@ public class Jugador {
     }
 
     private void condicionesIniciales() {
-
-        PlazaCentral plazaCentral = new EdificiosFabrica().crearPlazaCentral();
-        for (int i = 0; i < 3; i++) {
-            plebe.agregarAldeano(plazaCentral.construirAldeano());
-        }
-        this.castillo = new EdificiosFabrica().crearCastillo();
-        this.construcciones.agregarEdificio(plazaCentral);
         this.bolsaDeOro = oroInicial;
+        if(!colocarPosicionablesEnPosicionA()) {
+            colocarPosicionablesEnPosicionB();
+        }
+    }
+
+    private boolean colocarPosicionablesEnPosicionA(){
+        // Creo los posicionables
+        Posicion posPlazaCentral = new PosicionCuadrado(8,8,9,7);
+        PlazaCentral plazaCentral = new PlazaCentral(posPlazaCentral, new UnidadesFabrica());
+
+        try{
+            this.mapa.posicionar(plazaCentral);
+
+        }catch(NoPuedeColocar2IPosicionablesEnLaMismaPosicionException e){
+            return false;
+        }
+
+        // Cre posiciones y le asigno las posiciones a los edificios/aldeanos
+        Posicion posCastillo = new PosicionCuadrado(5,5,8,2);
+        Castillo castillo = new Castillo(posCastillo,new UnidadesFabrica(), new EstrategiaAtaqueCastillo());
+
+        Posicion posAldeano1 =  new PosicionDeUnCasillero(this.mapa,8,10);
+        Posicion posAldeano2 = new PosicionDeUnCasillero(this.mapa,10,8);
+        Posicion posAldeano3 = new PosicionDeUnCasillero(this.mapa,10,6);
+
+        Aldeano aldeano1 = new Aldeano(posAldeano1);
+        Aldeano aldeano2 = new Aldeano(posAldeano2);
+        Aldeano aldeano3 = new Aldeano(posAldeano3);
+
+        this.mapa.posicionar(castillo);
+        this.mapa.posicionar(aldeano1);
+        this.mapa.posicionar(aldeano2);
+        this.mapa.posicionar(aldeano3);
+
+        //
+        this.castillo = castillo;
+        this.construcciones.agregarEdificio(plazaCentral);
+
+        this.plebe.agregarAldeano(aldeano1);
+        this.plebe.agregarAldeano(aldeano2);
+        this.plebe.agregarAldeano(aldeano3);
+
+        return true;
+    }
+
+    private void colocarPosicionablesEnPosicionB(){
+
+        Posicion posPlazaCentral = new PosicionCuadrado(20,8,21,7);
+        PlazaCentral plazaCentral = new PlazaCentral(posPlazaCentral, new UnidadesFabrica());
+
+        Posicion posCastillo = new PosicionCuadrado(20,5,23,2);
+        Castillo castillo = new Castillo(posCastillo,new UnidadesFabrica(), new EstrategiaAtaqueCastillo());
+
+        Posicion posAldeano1 =  new PosicionDeUnCasillero(this.mapa,20,10);
+        Posicion posAldeano2 = new PosicionDeUnCasillero(this.mapa,18,8);
+        Posicion posAldeano3 = new PosicionDeUnCasillero(this.mapa,17,7);
+
+        Aldeano aldeano1 = new Aldeano(posAldeano1);
+        Aldeano aldeano2 = new Aldeano(posAldeano2);
+        Aldeano aldeano3 = new Aldeano(posAldeano3);
+
+        this.mapa.posicionar(plazaCentral);
+        this.mapa.posicionar(castillo);
+        this.mapa.posicionar(aldeano1);
+        this.mapa.posicionar(aldeano2);
+        this.mapa.posicionar(aldeano3);
+
+
+        this.castillo = castillo;
+        this.construcciones.agregarEdificio(plazaCentral);
+
+        this.plebe.agregarAldeano(aldeano1);
+        this.plebe.agregarAldeano(aldeano2);
+        this.plebe.agregarAldeano(aldeano3);
     }
 
 
