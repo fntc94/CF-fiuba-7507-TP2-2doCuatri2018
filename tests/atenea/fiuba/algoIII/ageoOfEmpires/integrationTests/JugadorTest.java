692d0368c1c2c73252132c2d9b9d9f7f4d65e53f
Ricardo Brandan <brandanricardo@yahoo.com.ar>
2018-11-26 23:41:27 -0300

    Agrega mapa a los parametros de Jugador en las pruebas

--- tests/atenea/fiuba/algoIII/ageoOfEmpires/integrationTests/JugadorTest.java
+++ tests/atenea/fiuba/algoIII/ageoOfEmpires/integrationTests/JugadorTest.java
@@ -2,6 +2,7 @@ package atenea.fiuba.algoIII.ageoOfEmpires.integrationTests;
 
 import atenea.fiuba.algoIII.ageoOfEmpires.modelo.excepciones.OroInsuficienteException;
 import atenea.fiuba.algoIII.ageoOfEmpires.modelo.juego.Jugador;
+import atenea.fiuba.algoIII.ageoOfEmpires.modelo.posicion.Mapa;
 import org.junit.Test;
 
 public class JugadorTest {
@@ -9,7 +10,8 @@ public class JugadorTest {
 
     @Test (expected = OroInsuficienteException.class)
     public void crearUnJugadorGastarOroYQueNoSeaSuficienteTest() {
-        Jugador jugador = new Jugador();
+        Mapa mapa = new Mapa(20,30);
+        Jugador jugador = new Jugador(mapa);
         jugador.pagarCosto(120);
     }
 }
